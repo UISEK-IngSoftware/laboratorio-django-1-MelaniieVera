99060f144af630528662ca13f3879be030f739a9
melaniie97 <melaniievera.97@gmail.com>
2025-05-14 22:42:10 -0500

    Laboratorio 4 terminado

---
+++ pokedex/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    pokemons = ["Pikachu", "Charmander", "Squirtle"]
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({
+        'pokemons': pokemons
+    }, request))
+
+def pokemon_details(request, pokemon):
+    template = loader.get_template('pokemon_details.html')
+    return HttpResponse(template.render({
+        'pokemon': pokemon
+    }, request))
+# Create your views here.
